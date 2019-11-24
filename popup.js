6a61161b3fec323a89cc6df57eb8577c6b37a745
ewenw <wang.yuxu@husky.neu.edu>
2019-11-24 16:46:58 -0500

    mods

--- popup.js
+++ popup.js
@@ -1,8 +1,83 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 'use strict';
+
+
+
+chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
+  let url = stripUrl(tabs[0].url);
+  let info = getInfo(url, draw);
+});
+
+function draw(info) {
+  if (info['hostName'])
+    $("#host").html(info['hostName']);
+  if (info['serviceCount'])
+    $("#services").html(info['serviceCount'] + " services");
+  if (info['contentDeliverers'].length) 
+    $("#contentDeliverers").html("Content deliverers: " + readableList(info['contentDeliverers']));
+  if (info['trackers'] && info['trackers'].length) 
+    $("#trackers").html("Trackers found: " + readableList(info['trackers']));
+  $('.info').fadeIn(1000);
+}
+
+function getInfo(url, cb) {
+  let info = {};
+  get("https://builtwith.com/" + url, (page) => {
+    let serviceCount = $(page).find(".row .mb-2 .mt-2").length;
+    
+    let contentDeliverers = [];
+    $(page).find("h6:contains('Content Delivery Network')").parent().parent().parent().find(".text-dark").each(function() {
+      if($(this).text() != "Content Delivery Network")
+        contentDeliverers.push($(this).text());
+    });
+    
+    let trackers = [];
+    $(page).find("h6:contains('Analytics and Tracking')").parent().parent().parent().find(".text-dark").each(function() {
+      trackers.push($(this).text());
+    });
+
+    info['serviceCount'] = serviceCount;
+    info['contentDeliverers'] = contentDeliverers;
+    info['trackers'] = trackers;
+
+    get("https://check-host.net/check-info?host=" + url, (page) => {
+      let hostName = $(page).find(".ipinfo-item").find(".hostinfo").find("tbody").children().eq(3).children().eq(1).html() || "Unknown";
+      info['hostName'] = hostName;
+
+
+      cb(info);
+    });
+  });
+}
+
+
+function getISPInfo(url) {
+  let xhr = createCORSRequest("https://check-host.net/check-info?host=" + url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      let el = $('<div></div>').append($.parseHTML(xhr.responseText));
+      
+    }
+  }
+  xhr.send();
+}
+
+
+
+
+
+
+
+
+// Helpers
+function get(url, cb) {
+  let xhr = createCORSRequest(url, true);
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      cb($('<div></div>').append($.parseHTML(xhr.responseText)));
+    }
+  }
+  xhr.send();
+}
 
 function createCORSRequest(url) {
   let method = "GET";
@@ -21,71 +96,19 @@ function createCORSRequest(url) {
   return xhr;
 }
 
-chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-  let url = stripUrl(tabs[0].url);
-  let hostName = getISPInfo(url);
-  let services = getServicesInfo(url);
-});
-
 function stripUrl(url) {
   return url_domain(url).replace(/(^\w+:|^)\/\//, '');
 }
-/*
-function getServicesInfo(url) {
-  let xhr = createCORSRequest("https://w3techs.com/sites/info/stackoverflow.com", true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      let el = $('<div></div>').append($.parseHTML(xhr.responseText));
-      let serviceCount = $(el).find("col-md-8").children().length;
-      $("#services").html(serviceCount + " services");
-      
-    }
-  }
-  xhr.send();
-}*/
 
 function url_domain(data) {
-  var    a      = document.createElement('a');
-         a.href = data;
+  var a = document.createElement('a');
+  a.href = data;
   return a.hostname;
 }
 
-function getServicesInfo(url) {
-  let xhr = createCORSRequest("https://builtwith.com/" + url, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      let el = $('<div></div>').append($.parseHTML(xhr.responseText));
-
-      let serviceCount = $(el).find(".row .mb-2 .mt-2").length;
-      $("#services").html(serviceCount + " services");
-      
-      let contentDeliverers = [];
-      $(el).find("h6:contains('Content Delivery Network')").parent().parent().parent().find(".text-dark").each(function() {
-        if($(this).text() != "Content Delivery Network")
-          contentDeliverers.push($(this).text());
-      });
-      if (contentDeliverers.length) 
-        $("#contentDeliverers").html("Content deliverers: " + contentDeliverers.join(", "));
-
-      let trackers = [];
-      $(el).find("h6:contains('Analytics and Tracking')").parent().parent().parent().find(".text-dark").each(function() {
-        trackers.push($(this).text());
-      });
-      if (trackers.length) 
-        $("#trackers").html("Trackers found: " + trackers.join(", "));
-    }
-  }
-  xhr.send();
-}
-
-function getISPInfo(url) {
-  let xhr = createCORSRequest("https://check-host.net/check-info?host=" + url, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      let el = $('<div></div>').append($.parseHTML(xhr.responseText));
-      let hostName = $(el).find(".ipinfo-item").find(".hostinfo").find("tbody").children().eq(3).children().eq(1).html() || "Unknown";
-      $("#host").html(hostName);
-    }
-  }
-  xhr.send();
+function readableList(arr) {
+  if (arr.length <= 3)
+    return arr.join(", ")
+  
+  return arr.slice(0, 2) + " and " + arr.length + " others";
 }
